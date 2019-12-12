9a0cdef823f9cd0fbcbb967d58080a46fd796b61
bkalafat <kalafatburak@gmail.com>
2019-12-12 23:54:02 +0300

    feat: Show steppers and sliders by type of item

--- App.js
+++ App.js
@@ -2,8 +2,10 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { getMetricMetaInfo } from './utils/helpers'
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View>
