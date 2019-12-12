ad35fae2eb2fcdf2caf1a5c600a0d54f985964e0
bkalafat <kalafatburak@gmail.com>
2019-12-12 18:13:46 +0300

    feat: Add get metric meta info method by metric.

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
 
 export default class App extends React.Component {
 
-  componentDidMount() {
-    console.log('Before')
-    debugger
-    console.log('After')
-  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Burak Open up App.js to start working on your app!</Text>
+        <Ionicons name='ios-pizza' color='red' size={100} />
       </View>
     );
   }
