90c10a14956e9dfae946dace8cfcdec1c34bd05d
bkalafat <kalafatburak@gmail.com>
2020-01-16 14:36:26 +0300

    feat: Add history component and show bare data.

---
+++ components/History.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { recieveEntries, addEntry } from '../actions'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import { fetchCalendarResults } from '../utils/api'
+import entries from '../reducers'
+
+class History extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+
+    fetchCalendarResults()
+      .then((entries) => dispatch(recieveEntries(entries)))
+      .then(({entries}) => {
+        if (!entries[timeToString()]) {
+          dispatch(addEntry({
+            [timeToString()]: getDailyReminderValue()
+          }))
+        }
+      } )
+  }
+  render() {
+    return (
+      <View>
+        <Text>{JSON.stringify(this.props)}</Text>
+      </View>
+    )
+  }
+}
+
+function mapStateToProps (entries) {
+  return {
+    entries
+  }
+}
+
+export default connect(mapStateToProps)(History)
