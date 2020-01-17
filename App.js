4d8d470a3d4f58903858ed9f0c4f2cfde5163b04
bkalafat <kalafatburak@gmail.com>
2020-01-17 10:51:08 +0300

    feat: Add tab navigator.

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { purple, white } from './utils/colors'
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
+
+const TabNavigator = createMaterialTopTabNavigator(
+  {
+    History: History,
+    AddEntry: AddEntry,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return routeName === 'History' ? (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        ) : (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        );
+      },
+    }),
+    tabBarOptions: {
+      showIcon: true,
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 66,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    },
+  }
+);
+
+const Tabs = createAppContainer(TabNavigator)
 
 export default class App extends React.Component {
   render() {
@@ -13,7 +55,7 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }} >
           <View style={{ height: 20 }} />
-          <History />
+          <Tabs />
         </View>
       </Provider>
     )
